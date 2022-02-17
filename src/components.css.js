2d4ab06e63b5f9492a31b0911dbd718dc1e7b220
DavidNogu <david@dexper.io>
2022-02-17 19:29:54 +0100

    ✨ Initial commit

---
+++ src/components.css.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  flex-wrap: nowrap;
+  overflow-x: auto;
+`;
+export const Board = styled.div`
+  display: flex;
+  width: fit-content;
+`;
+export const Column = styled.div`
+  width: 240px;
+  background: #333;
+  border: 0.1px #aaa solid;
+  color: white;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+`;
+export const MovingLine = styled.div`
+  z-index: 399;
+  align-self: center;
+  width: 1px;
+  height: -webkit-fill-available;
+  background: orange;
+  position: absolute;
+  margin-left: ${({ minutes }) =>
+    `${((minutes.getMinutes() / 60) * 100).toFixed(1)}%`};
+`;
+export const ColumnHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
